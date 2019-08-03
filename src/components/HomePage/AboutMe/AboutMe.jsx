b5163a61ce03e8f46368004573428fb0a521da26
Cristian <communication-world@live.com>
2019-08-03 15:56:53 +0100

    Fixing small

--- src/components/HomePage/AboutMe/AboutMe.jsx
+++ src/components/HomePage/AboutMe/AboutMe.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import s from './AboutMe.scss';
-import squareCat from '../../../../static/cat.jpg';
+import heroSmall from '../../../../static/hero-small.png';
 
 class AboutMe extends React.Component {
 	constructor(props) {
@@ -12,14 +12,14 @@ class AboutMe extends React.Component {
 		const { aboutMe } = this.props;
 		return (
 			<div id="aboutMe" className={`container-fluid ${s.containerBackground}`}>
-				<div className="row h-100">
+				<div className={`row h-100 ${s.clearDisplay}`}>
 					<div className="col my-auto p-0 text-center">
 						<h2 className="h1 mb-4">{aboutMe && aboutMe.title && aboutMe.title}</h2>
 						<hr />
 						<h3 className="h4 pl-5 pr-5 mt-2">{aboutMe && aboutMe.subTitle && aboutMe.subTitle}</h3>
 					</div>
 					<div className="col my-auto text-center">
-						<img className="rounded-circle" src={squareCat} />
+						<img className="rounded-circle" src={heroSmall} />
 					</div>
 				</div>
 			</div>
