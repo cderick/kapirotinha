1524c95adce22fd828f43a94b8a0bc8a55931a14
Cristian <communication-world@live.com>
2019-08-07 22:04:39 +0100

    Change structure to accept Apps looping

--- src/components/HomePage/MyWorks/PopupContent/PopupContent.jsx
+++ src/components/HomePage/MyWorks/PopupContent/PopupContent.jsx
@@ -14,17 +14,17 @@ class PopupContent extends React.Component {
         this.escFunction = this.escFunction.bind(this);
     }
 
-    escFunction(event){
-        if(event.keyCode === 27) {
+    escFunction(event) {
+        if (event.keyCode === 27) {
             this.props.handlePopupOverlay()
         }
     }
 
-    componentDidMount(){
+    componentDidMount() {
         document.addEventListener('keydown', this.escFunction, false);
     }
-    
-    componentWillUnmount(){
+
+    componentWillUnmount() {
         document.removeEventListener('keydown', this.escFunction, false);
     }
 
@@ -57,100 +57,104 @@ class PopupContent extends React.Component {
         const { targetCollapse, targetOn } = this.state;
         return (
             <div id="pop-up-one" className={`${s.popUpOverlay} ${s.popUpOverlaytarget}`}>
-                {webWorks &&
+                {webWorks && webWorks.length &&
                     <div className={s.popUp} id="accordion">
-                        <a className="close" onClick={() => this.props.handlePopupOverlay()} href="javascript:void(0);">&times;</a>
-                        <h1 className="mb-2">{webWorks.mainTitle}</h1>
-                        <hr className="mb-3 pb-0" />
-                        <h4 className="pb-4">{webWorks.descritpion}</h4>
-                        {webWorks.allWorks && webWorks.allWorks.map((cv, ind) =>
-                            <div key={`${cv.uniQueId}${ind}`} className={`${s.mainComponent} ${s[cv.uniQueId]} mb-5 ${targetCollapse === cv.uniQueId && targetOn == 'true' && s.autoHeight}`}>
-                                <div id={cv.uniQueId} className={`${s.bottomContainer} ${targetCollapse === cv.uniQueId && targetOn == 'true' && s.positionSmooth}`} onClick={(e) => {
-                                    const id = e.target.id;
-                                    const att = e.target.getAttribute('aria-expanded');
-                                    this.setState({
-                                        targetCollapse: id,
-                                        targetOn: att
-                                    }, () => {
-                                        console.log(this.state.targetCollapse)
-                                        setTimeout(() => {
-                                            if (this.state.targetOn == 'true') {
-                                                this.scroolHandle(id);
-                                            }
-                                        }, 400);
-                                    });
-                                }} data-toggle="collapse" data-target={`#collapse${cv.uniQueId}`} aria-expanded="false" aria-controls={`collapse${cv.uniQueId}`}>
-                                    <h2 className="mb-0 pl-4 float-left">{cv.workName}</h2>
-                                    <h2 className="mb-0 pr-4 float-right">Click to expand <i className="fas fa-chevron-circle-down"></i></h2>
-                                </div>
-                                <div className="collapse" data-parent="#accordion" id={`collapse${cv.uniQueId}`}>
-                                    <div className={s.contentPopup}>
-                                        <img src={require(`../../../../../static/${cv.bannerImage}`)} className={s.resizeImage} />
-                                        <div className="row mt-4">
-                                            <div className="col">
-                                                <h2 className="text-center">{cv.collorPallete.mainTitle}</h2>
-                                                <div className="row">
-                                                    <div className="col pt-4">
-                                                        {cv.collorPallete.left &&
-                                                            cv.collorPallete.left.map((cpl, indcpl) =>
-                                                                <div key={`left${cpl.name}${indcpl}`} className="pb-3">
-                                                                    <div className={s.colorRounded} style={{ border: `1px solid ${cpl.color}`, backgroundColor: `${cpl.color}` }}>&nbsp;</div>
-                                                                    <p className="pt-3 mb-0">{cpl.color}</p>
-                                                                    <p>{cpl.name}</p>
-                                                                </div>
-                                                            )}
-                                                    </div>
-                                                    <div className="col pt-4">
-                                                        {cv.collorPallete.right &&
-                                                            cv.collorPallete.right.map((cpr, indcpr) =>
-                                                                <div key={`right${cpr.name}${indcpr}`} className="pb-3">
-                                                                    <div className={s.colorRounded} style={{ border: `1px solid ${cpr.color}`, backgroundColor: `${cpr.color}` }}>&nbsp;</div>
-                                                                    <p className="pt-3 mb-0">{cpr.color}</p>
-                                                                    <p>{cpr.name}</p>
-                                                                </div>
-                                                            )}
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div className="col">
-                                                <h2 className="text-center">{cv.typoGraphy.mainTitle}</h2>
-                                                <p className="pt-4">{cv.typoGraphy.description}</p>
-                                                <div className="row">
+                        {webWorks.map((wb, indwb) =>
+                            <section key={`${wb.mainTitle}${indwb}`}>
+                                <a className="close" onClick={() => this.props.handlePopupOverlay()} href="javascript:void(0);">&times;</a>
+                                <h1 className="mb-2">{wb.mainTitle}</h1>
+                                <hr className="mb-3 pb-0" />
+                                <h4 className="pb-4">{wb.descritpion}</h4>
+                                {wb.allWorks && wb.allWorks.map((cv, ind) =>
+                                    <div key={`${cv.uniQueId}${ind}`} className={`${s.mainComponent} ${s[cv.uniQueId]} mb-5 ${targetCollapse === cv.uniQueId && targetOn == 'true' && s.autoHeight}`}>
+                                        <div id={cv.uniQueId} className={`${s.bottomContainer} ${targetCollapse === cv.uniQueId && targetOn == 'true' && s.positionSmooth}`} onClick={(e) => {
+                                            const id = e.target.id;
+                                            const att = e.target.getAttribute('aria-expanded');
+                                            this.setState({
+                                                targetCollapse: id,
+                                                targetOn: att
+                                            }, () => {
+                                                console.log(this.state.targetCollapse)
+                                                setTimeout(() => {
+                                                    if (this.state.targetOn == 'true') {
+                                                        this.scroolHandle(id);
+                                                    }
+                                                }, 400);
+                                            });
+                                        }} data-toggle="collapse" data-target={`#collapse${cv.uniQueId}`} aria-expanded="false" aria-controls={`collapse${cv.uniQueId}`}>
+                                            <h2 className="mb-0 pl-4 float-left">{cv.workName}</h2>
+                                            <h2 className="mb-0 pr-4 float-right">Click to expand <i className="fas fa-chevron-circle-down"></i></h2>
+                                        </div>
+                                        <div className="collapse" data-parent="#accordion" id={`collapse${cv.uniQueId}`}>
+                                            <div className={s.contentPopup}>
+                                                <img src={require(`../../../../../static/${cv.bannerImage}`)} className={s.resizeImage} />
+                                                <div className="row mt-4">
                                                     <div className="col">
-                                                        {cv.typoGraphy.left &&
-                                                            cv.typoGraphy.left.map((ctl, indctl) =>
-                                                                <div key={`left${ctl.name}${indctl}`} className="pb-3">
-                                                                    <h1 className={ctl.classes}>{ctl.type}</h1>
-                                                                    <p className={ctl.sizeClass}>{ctl.size}</p>
-                                                                    <p>{ctl.name}</p>
-                                                                </div>
-                                                            )}
+                                                        <h2 className="text-center">{cv.collorPallete.mainTitle}</h2>
+                                                        <div className="row">
+                                                            <div className="col pt-4">
+                                                                {cv.collorPallete.left &&
+                                                                    cv.collorPallete.left.map((cpl, indcpl) =>
+                                                                        <div key={`left${cpl.name}${indcpl}`} className="pb-3">
+                                                                            <div className={s.colorRounded} style={{ border: `1px solid ${cpl.color}`, backgroundColor: `${cpl.color}` }}>&nbsp;</div>
+                                                                            <p className="pt-3 mb-0">{cpl.color}</p>
+                                                                            <p>{cpl.name}</p>
+                                                                        </div>
+                                                                    )}
+                                                            </div>
+                                                            <div className="col pt-4">
+                                                                {cv.collorPallete.right &&
+                                                                    cv.collorPallete.right.map((cpr, indcpr) =>
+                                                                        <div key={`right${cpr.name}${indcpr}`} className="pb-3">
+                                                                            <div className={s.colorRounded} style={{ border: `1px solid ${cpr.color}`, backgroundColor: `${cpr.color}` }}>&nbsp;</div>
+                                                                            <p className="pt-3 mb-0">{cpr.color}</p>
+                                                                            <p>{cpr.name}</p>
+                                                                        </div>
+                                                                    )}
+                                                            </div>
+                                                        </div>
                                                     </div>
                                                     <div className="col">
-                                                        {cv.typoGraphy.right &&
-                                                            cv.typoGraphy.right.map((ctr, indctr) =>
-                                                                <div key={`right${ctr.name}${indctr}`} className="pb-3">
-                                                                    <h1 className={ctr.classes}>{ctr.type}</h1>
-                                                                    <p className={ctr.sizeClass}>{ctr.size}</p>
-                                                                    <p>{ctr.name}</p>
-                                                                </div>
-                                                            )}
+                                                        <h2 className="text-center">{cv.typoGraphy.mainTitle}</h2>
+                                                        <p className="pt-4">{cv.typoGraphy.description}</p>
+                                                        <div className="row">
+                                                            <div className="col">
+                                                                {cv.typoGraphy.left &&
+                                                                    cv.typoGraphy.left.map((ctl, indctl) =>
+                                                                        <div key={`left${ctl.name}${indctl}`} className="pb-3">
+                                                                            <h1 className={ctl.classes}>{ctl.type}</h1>
+                                                                            <p className={ctl.sizeClass}>{ctl.size}</p>
+                                                                            <p>{ctl.name}</p>
+                                                                        </div>
+                                                                    )}
+                                                            </div>
+                                                            <div className="col">
+                                                                {cv.typoGraphy.right &&
+                                                                    cv.typoGraphy.right.map((ctr, indctr) =>
+                                                                        <div key={`right${ctr.name}${indctr}`} className="pb-3">
+                                                                            <h1 className={ctr.classes}>{ctr.type}</h1>
+                                                                            <p className={ctr.sizeClass}>{ctr.size}</p>
+                                                                            <p>{ctr.name}</p>
+                                                                        </div>
+                                                                    )}
+                                                            </div>
+                                                        </div>
                                                     </div>
                                                 </div>
+                                                {cv.bannerSections &&
+                                                    cv.bannerSections.map((bs, indbs) =>
+                                                        <div key={`${bs.mainTitle}${indbs}`} className="row mt-4 pb-5" style={{ backgroundColor: `${bs.background ? bs.background : '#fff'}` }}>
+                                                            <div className="col">
+                                                                <h2 className="mb-5 mt-5 text-center h1">{bs.mainTitle}</h2>
+                                                                <img src={require(`../../../../../static/${bs.images}`)} className={s.resizeImage} />
+                                                            </div>
+                                                        </div>
+                                                    )}
                                             </div>
                                         </div>
-                                        {cv.bannerSections &&
-                                            cv.bannerSections.map((bs, indbs) =>
-                                                <div key={`${bs.mainTitle}${indbs}`} className="row mt-4 pb-5" style={{ backgroundColor: `${bs.background ? bs.background : '#fff'}` }}>
-                                                    <div className="col">
-                                                        <h2 className="mb-5 mt-5 text-center h1">{bs.mainTitle}</h2>
-                                                        <img src={require(`../../../../../static/${bs.images}`)} className={s.resizeImage} />
-                                                    </div>
-                                                </div>
-                                            )}
                                     </div>
-                                </div>
-                            </div>
+                                )}
+                            </section>
                         )}
                     </div>
                 }
