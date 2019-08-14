fbec849b066f1d9d2d3311d96661dd4a21b3e543
Cristian <communication-world@live.com>
2019-08-14 21:35:39 +0100

    Nice fix on Contact me

--- src/components/HomePage/ContactMe/ContactMe.jsx
+++ src/components/HomePage/ContactMe/ContactMe.jsx
@@ -15,8 +15,8 @@ class ContactMe extends React.Component {
 					<div className={`${s.paddingNoneSm} col my-auto text-center pl-0`}>
 						<h2 className="h1 mb-3">{contactMe && contactMe.title && contactMe.title}</h2>
 						<hr />
-						<h3 className="h4 mt-3"><i className="fas fa-phone"></i> &nbsp; {contactMe && contactMe.phoneNumer && contactMe.phoneNumer}</h3>
-						<h3 className="h4"><i className="fas fa-envelope-open-text"></i> &nbsp; {contactMe && contactMe.emailAddress && contactMe.emailAddress}</h3>
+						<h3 className="h4 mt-3"><a className={s.contactmeLinks} href={`tel:${contactMe && contactMe.phoneNumer && contactMe.phoneNumer.trim()}`}><i className="fas fa-phone"></i> &nbsp; {contactMe && contactMe.phoneNumer && contactMe.phoneNumer}</a></h3>
+						<h3 className="h4"><a className={s.contactmeLinks} href={`mailto:${contactMe && contactMe.emailAddress && contactMe.emailAddress}`}><i className="fas fa-envelope-open-text"></i> &nbsp; {contactMe && contactMe.emailAddress && contactMe.emailAddress}</a></h3>
 					</div>
 					<div className={`${s.paddingNoneSm} col my-auto text-center pr-0`}>
 						<h2 className="h1 mb-3">{contactMe && contactMe.socialTitle && contactMe.socialTitle}</h2>
@@ -27,7 +27,7 @@ class ContactMe extends React.Component {
 									<div key={`socialId${ind}`} className="col p-0 mt-3">
 										<p className="h1">
 											<a href={cv.url} target="_blank">
-												<i className={`text-light ${cv.icon}`}></i>
+												<i className={`text-light ${s.iconHover} ${cv.icon}`}></i>
 											</a>
 										</p>
 									</div>
