941855d738f710fad9babd80302433be5f5a75f2
Cristian <communication-world@live.com>
2019-08-19 22:13:26 +0100

    Final changes on SCSS mobile adjust

--- src/components/HomePage/MyWorks/PopupContent/ArtContent/ArtContent.jsx
+++ src/components/HomePage/MyWorks/PopupContent/ArtContent/ArtContent.jsx
@@ -19,29 +19,31 @@ class ArtContent extends React.Component {
                 mainTarget.children[0].click();
                 this.setState({
                     activeList: mainTarget.id,
+                }, () => {
+                    this.equalHeights();
                 });
             } else {
                 this.setState({
                     activeList: mainTarget.parentElement.id,
+                }, () => {
+                    this.equalHeights();
                 });
             }
         }
     }
 
     equalHeights() {
-		let findClass = document.getElementsByClassName('equalHeights');
-		let tallest = 0;
+        let findClass = document.getElementsByClassName('equalHeights');
+        const finalHeight = document.getElementsByClassName('tab-content')[0].offsetHeight;
+        const getMinHeight = document.getElementsByClassName('grabMinheight')[0].offsetHeight;
 		for (let i = 0; i < findClass.length; i++) {
-			const ele = findClass[i];
-			const eleHeight = ele.offsetHeight;
-			tallest = (eleHeight > tallest ? eleHeight : tallest);
-		}
-		for (let i = 0; i < findClass.length; i++) {
-			findClass[i].style.height = `${tallest}px`;
+            findClass[i].style.minHeight = `${getMinHeight}px`;
+			findClass[i].style.height = `${finalHeight}px`;
 		}
 	}
 
     componentDidMount() {
+        window.addEventListener('resize', this.equalHeights);
         $('.carousel').carousel();
         setTimeout(() => {
             this.equalHeights();
@@ -53,9 +55,9 @@ class ArtContent extends React.Component {
         const { activeList } = this.state;
         return (
             <div id="pop-up-one" className={`${s.popUpOverlay} ${s.popUpOverlaytarget}`}>
-                <a className={`close ${s.myClose}`} onClick={() => this.props.handlePopupOverlay()} href="javascript:void(0);">&times;</a>
                 <div className={s.popUp}>
-                    <div className="row h-100">
+                <a className={`close ${s.myClose}`} onClick={() => this.props.handlePopupOverlay()} href="javascript:void(0);">&times;</a>
+                    <div className="row grabMinheight h-100">
                         <div className={`col-3 p-0 equalHeights ${s.panelBackground}`}>
                             <ul className={`nav ${s.tabsLeft} sideways`}>
                                 {artWorks && artWorks.length && artWorks.map((cv, ind) => (
@@ -63,8 +65,8 @@ class ArtContent extends React.Component {
                                 ))}
                             </ul>
                         </div>
-                        <div className="col-9 equalHeights">
-                            <div className="tab-content p-5">
+                        <div className={`col-9 d-table equalHeights ${s.removeMobile}`}>
+                            <div className="tab-content d-table-cell align-middle p-5">
                                 {artWorks && artWorks.length && artWorks.map((av, ix) => (
                                 <div key={`imagesArt${ix}`} className={`tab-pane ${ix === 0 ? 'active' : ''}`} id={av.title}>
                                     <div id={`mainCarousel${ix}`} className={`${s.carouselSpec} carousel slide`} data-ride="carousel">
@@ -87,6 +89,31 @@ class ArtContent extends React.Component {
                                 </div>
                                 ))}
                             </div>
+                        </div>
+                        <div className={`col pb-5 ${s.mobileOnly}`}>
+                            <a className={`close ${s.myCloseMobile}`} onClick={() => this.props.handlePopupOverlay()} href="javascript:void(0);">&times;</a>
+                            {artWorks && artWorks.length && artWorks.map((av, ix) => (
+                                <div key={`imagesArt${ix}`} id={`mobile${av.title}`}>
+                                    <h2 className={`h2 pt-5 text-center pb-3 ${s.titleMobile}`}>{av.title}</h2>
+                                    <div id={`mobileCarousel${ix}`} className={`${s.carouselSpec} carousel slide`} data-ride="carousel">
+                                        <div className="carousel-inner pl-5 pr-5">
+                                            {av.images && av.images.length && av.images.map((img, iImg) => (
+                                            <div key={`img${iImg}`} className={`carousel-item ${iImg === 0 ? 'active' : ''}`}>  
+                                                <img  className="d-block w-100" src={require(`../../../../../../static/${img.data}`)} alt={img.data} />
+                                            </div>
+                                            ))}
+                                        </div>
+                                        <a className={`${s.carouseControl} ${s.carouselPrev} carousel-control-prev`} href={`#mobileCarousel${ix}`} role="button" data-slide="prev">
+                                            <span className={`carousel-control-prev-icon ${s.controlLeft}`} aria-hidden="true"></span>
+                                            <span className="sr-only">Previous</span>
+                                        </a>
+                                        <a className={`${s.carouseControl} ${s.carouselNext} carousel-control-next`} href={`#mobileCarousel${ix}`} role="button" data-slide="next">
+                                            <span className={`carousel-control-next-icon ${s.controlRight}`} aria-hidden="true"></span>
+                                            <span className="sr-only">Next</span>
+                                        </a>
+                                    </div>
+                                </div>
+                            ))}
                         </div>
                         <div className="clearfix"></div>
                     </div>
