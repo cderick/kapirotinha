1674297e54577829f27b734aa55a1c73613d455f
Cristian <communication-world@live.com>
2019-11-07 20:44:11 +0000

    Include support for video and include new section Motion

--- src/components/HomePage/MyWorks/PopupContent/ArtContent/ArtContent.jsx
+++ src/components/HomePage/MyWorks/PopupContent/ArtContent/ArtContent.jsx
@@ -12,10 +12,10 @@ class ArtContent extends React.Component {
         this.equalHeights = this.equalHeights.bind(this);
     }
 
-    handleClick(e){
-        if(e){
+    handleClick(e) {
+        if (e) {
             const mainTarget = e.target;
-            if(mainTarget && mainTarget.children.length){
+            if (mainTarget && mainTarget.children.length) {
                 mainTarget.children[0].click();
                 this.setState({
                     activeList: mainTarget.id,
@@ -36,11 +36,11 @@ class ArtContent extends React.Component {
         let findClass = document.getElementsByClassName('equalHeights');
         const finalHeight = document.getElementsByClassName('tab-content')[0].offsetHeight;
         const getMinHeight = document.getElementsByClassName('grabMinheight')[0].offsetHeight;
-		for (let i = 0; i < findClass.length; i++) {
+        for (let i = 0; i < findClass.length; i++) {
             findClass[i].style.minHeight = `${getMinHeight}px`;
-			findClass[i].style.height = `${finalHeight}px`;
-		}
-	}
+            findClass[i].style.height = `${finalHeight}px`;
+        }
+    }
 
     componentDidMount() {
         window.addEventListener('resize', this.equalHeights);
@@ -56,7 +56,7 @@ class ArtContent extends React.Component {
         return (
             <div id="pop-up-one" className={`${s.popUpOverlay} ${s.popUpOverlaytarget}`}>
                 <div className={s.popUp}>
-                <a className={`close ${s.myClose}`} onClick={() => this.props.handlePopupOverlay()} href="javascript:void(0);">&times;</a>
+                    <a className={`close ${s.myClose}`} onClick={() => this.props.handlePopupOverlay()} href="javascript:void(0);">&times;</a>
                     <div className="row grabMinheight h-100">
                         <div className={`col-3 p-0 equalHeights ${s.panelBackground}`}>
                             <ul className={`nav ${s.tabsLeft} sideways`}>
@@ -68,25 +68,31 @@ class ArtContent extends React.Component {
                         <div className={`col-9 d-table equalHeights ${s.removeMobile}`}>
                             <div className="tab-content d-table-cell align-middle p-5">
                                 {artWorks && artWorks.length && artWorks.map((av, ix) => (
-                                <div key={`imagesArt${ix}`} className={`tab-pane ${ix === 0 ? 'active' : ''}`} id={av.title}>
-                                    <div id={`mainCarousel${ix}`} className={`${s.carouselSpec} carousel slide`} data-ride="carousel">
-                                        <div className="carousel-inner pl-5 pr-5">
-                                            {av.images && av.images.length && av.images.map((img, iImg) => (
-                                            <div key={`img${iImg}`} className={`carousel-item ${iImg === 0 ? 'active' : ''}`}>  
-                                                <img  className="d-block w-100" src={require(`../../../../../../static/${img.data}`)} alt={img.data} />
+                                    <div key={`imagesArt${ix}`} className={`tab-pane ${ix === 0 ? 'active' : ''}`} id={av.title}>
+                                        <div id={`mainCarousel${ix}`} className={`${s.carouselSpec} carousel slide`} data-ride="carousel">
+                                            <div className="carousel-inner pl-5 pr-5">
+                                                {av.images && av.images.length && av.images.map((img, iImg) => (
+                                                    <div key={`img${iImg}`} className={`carousel-item ${iImg === 0 ? 'active' : ''}`}>
+                                                        {av.title === 'Motion' ? (
+                                                            <video autoPlay loop className="d-block w-100">
+                                                                <source src={require(`../../../../../../static/${img.data}`)} type="video/mp4" />
+                                                            </video>
+                                                        ) :
+                                                            <img className="d-block w-100" src={require(`../../../../../../static/${img.data}`)} alt={img.data} />
+                                                        }
+                                                    </div>
+                                                ))}
                                             </div>
-                                            ))}
+                                            <a className={`${s.carouseControl} ${s.carouselPrev} carousel-control-prev`} href={`#mainCarousel${ix}`} role="button" data-slide="prev">
+                                                <span className={`carousel-control-prev-icon ${s.controlLeft}`} aria-hidden="true"></span>
+                                                <span className="sr-only">Previous</span>
+                                            </a>
+                                            <a className={`${s.carouseControl} ${s.carouselNext} carousel-control-next`} href={`#mainCarousel${ix}`} role="button" data-slide="next">
+                                                <span className={`carousel-control-next-icon ${s.controlRight}`} aria-hidden="true"></span>
+                                                <span className="sr-only">Next</span>
+                                            </a>
                                         </div>
-                                        <a className={`${s.carouseControl} ${s.carouselPrev} carousel-control-prev`} href={`#mainCarousel${ix}`} role="button" data-slide="prev">
-                                            <span className={`carousel-control-prev-icon ${s.controlLeft}`} aria-hidden="true"></span>
-                                            <span className="sr-only">Previous</span>
-                                        </a>
-                                        <a className={`${s.carouseControl} ${s.carouselNext} carousel-control-next`} href={`#mainCarousel${ix}`} role="button" data-slide="next">
-                                            <span className={`carousel-control-next-icon ${s.controlRight}`} aria-hidden="true"></span>
-                                            <span className="sr-only">Next</span>
-                                        </a>
                                     </div>
-                                </div>
                                 ))}
                             </div>
                         </div>
@@ -98,9 +104,9 @@ class ArtContent extends React.Component {
                                     <div id={`mobileCarousel${ix}`} className={`${s.carouselSpec} carousel slide`} data-ride="carousel">
                                         <div className="carousel-inner pl-5 pr-5">
                                             {av.images && av.images.length && av.images.map((img, iImg) => (
-                                            <div key={`img${iImg}`} className={`carousel-item ${iImg === 0 ? 'active' : ''}`}>  
-                                                <img  className="d-block w-100" src={require(`../../../../../../static/${img.data}`)} alt={img.data} />
-                                            </div>
+                                                <div key={`img${iImg}`} className={`carousel-item ${iImg === 0 ? 'active' : ''}`}>
+                                                    <img className="d-block w-100" src={require(`../../../../../../static/${img.data}`)} alt={img.data} />
+                                                </div>
                                             ))}
                                         </div>
                                         <a className={`${s.carouseControl} ${s.carouselPrev} carousel-control-prev`} href={`#mobileCarousel${ix}`} role="button" data-slide="prev">
